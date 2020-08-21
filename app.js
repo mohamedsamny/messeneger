c384e81cad1a9d0f85a779bff4d6508211f28d7f
Mohamed Samny <mohamedsamny@gmail.com>
2020-08-21 17:32:15 -0400

    Setup mongoose access

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 // jshint esversion:6
 
-const express = require ("express");
-const bodyParser = require ("body-parser");
-const routes = require ("./routes");
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+require("./models");
 
-const app = express();
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connection.on("connected", () => {
+  // We need to be connected to the DB for this service to work
+  finishSetup();
+})
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+mongoose.connection.on("error", (err) => {
+  console.log("Error while connecting to MongoDB!");
+})
 
-app.set('view engine', 'ejs');
+mongoose.connect("mongodb://127.0.0.1/messenger", { useNewUrlParser: true, useUnifiedTopology: true });
 
-routes(app);
+function finishSetup() {
+  const app = express();
 
-app.listen(3000, function() {
-  console.log("Server started at port 3000");
-});
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(express.static("public"));
+
+  app.set("view engine", "ejs");
+
+  routes(app);
+
+  app.listen(3000, function() {
+    console.log("Server started at port 3000");
+  });
+};
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    required: true,
+    type: String
+  },
+  email: {
+    required: true,
+    type: String
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
